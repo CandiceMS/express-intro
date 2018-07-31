5e797c99bc848fbbb59a610a91b872120ee730f6
CandiceMS <32810632+CandiceMS@users.noreply.github.com>
2018-07-31 12:29:54 +0200

    Testing instructions

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
 res.render('home');
@@ -15,6 +24,3 @@ let PORT = process.env.PORT || 3007;
 app.listen(PORT, function(){
 console.log('App starting on port', PORT);
 });
-
-
-
